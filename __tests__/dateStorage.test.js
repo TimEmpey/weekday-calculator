6599ebe9bdf6c2baa9c979efe1b7b13dd59ac884
Tim Empey <timothyempey@gmail.com>
2022-08-31 13:17:47 -0700

    add passing test 2
    Joseph Jackson <josephjacksonw@users.noreply.github.com>

--- __tests__/dateStorage.test.js
+++ __tests__/dateStorage.test.js
@@ -3,9 +3,14 @@ import DateStorage from '../src/dateStorage.js';
 describe('DateStorage', () => {
 
   test('should correctly create a date object with month, day, and year', () => {
-    const input = new DateStorage("August", 31, 2022)
-    expect(DateStorage.month).toEqual("August");
-    expect(DateStorage.day).toEqual(31);
-    expect(DateStorage.year).toEqual(2022);
+    const input = new DateStorage("August", 31, 1976)
+    expect(input.month).toEqual("August");
+    expect(input.day).toEqual(31);
+    expect(input.year).toEqual(1976);
+  });
+  
+  test('should correctly determine starting day of the year and whether or not it was a leap year', () => {
+    const input = new DateStorage("August", 31, 1976)
+    expect(input.weekday()).toEqual([1, 4])
   });
 });
--- src/dateStorage.js
+++ src/dateStorage.js
@@ -1,3 +1,50 @@
 export default function DateStorage(month, day, year) {
-
+  this.month = month;
+  this.day = day;
+  this.year = year;
 };
+
+DateStorage.prototype.weekday = function(input) {
+  let yearStart = 0
+  let leapYear = 0
+  let cycle = (this.year - 12 ) % 28
+  /* istanbul ignore else */
+  if (cycle % 4 === 0) {
+    leapYear = 1
+    if (cycle === 12) {
+      yearStart = 0
+    } else if (cycle === 24) {
+      yearStart = 1
+    } else if (cycle === 8) {
+      yearStart = 2
+    } else if (cycle === 20) {
+      yearStart = 3
+    } else if (cycle === 4) {
+      yearStart = 4
+    } else if (cycle === 16) {
+      yearStart = 5
+    } else if (cycle === 0) {
+      yearStart = 6
+    }
+    let arr = [leapYear, yearStart]
+    return arr
+  } else {
+    if (cycle === 6 || cycle === 17 || cycle === 23) {
+      yearStart = 0
+    } else if(cycle === 1 || cycle === 7 || cycle === 18) {
+      yearStart = 1
+    } else if(cycle === 2 || cycle === 13 || cycle === 19) {
+      yearStart = 2
+    } else if(cycle === 3 || cycle === 14 || cycle === 25) {
+      yearStart = 3
+    } else if(cycle === 9 || cycle === 15 || cycle === 26) {
+      yearStart = 4
+    } else if(cycle === 10 || cycle === 21 || cycle === 27) {
+      yearStart = 5
+    } else if(cycle === 5 || cycle === 11 || cycle === 22) {
+      yearStart = 6
+    }
+    let arr = [leapYear, yearStart]
+    return arr
+  }
+}
