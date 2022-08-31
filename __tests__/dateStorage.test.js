900e3bae18a26545682a79d3dde230f1cf040c5a
Tim Empey <timothyempey@gmail.com>
2022-08-31 15:08:48 -0700

    add passing test 3
    Joseph Jackson <josephjacksonw@gmail.com>

--- __tests__/dateStorage.test.js
+++ __tests__/dateStorage.test.js
@@ -1,16 +1,26 @@
 import DateStorage from '../src/dateStorage.js';
 
 describe('DateStorage', () => {
+  // let input;
+  
+    // beforeEach(() => {
+    //   input = new DateStorage(8, 31, 2022)
+    // });
 
   test('should correctly create a date object with month, day, and year', () => {
-    const input = new DateStorage("August", 31, 1976)
-    expect(input.month).toEqual("August");
+    let input = new DateStorage(8, 31, 2022, [])
+    expect(input.month).toEqual(8);
     expect(input.day).toEqual(31);
-    expect(input.year).toEqual(1976);
+    expect(input.year).toEqual(2022);
   });
   
   test('should correctly determine starting day of the year and whether or not it was a leap year', () => {
-    const input = new DateStorage("August", 31, 1976)
-    expect(input.weekday()).toEqual([1, 4])
+    let input = new DateStorage(8, 31, 2022, [])
+    expect(input.findYear()).toEqual([0, 6])
+  });
+
+  test('should return day of week based on month, day, year', () => {
+    let input = new DateStorage(8, 31, 2022, [0,6])
+    expect(input.findMonth()).toEqual(1)
   });
 });
--- src/dateStorage.js
+++ src/dateStorage.js
@@ -1,50 +1,101 @@
-export default function DateStorage(month, day, year) {
-  this.month = month;
-  this.day = day;
-  this.year = year;
-};
+export default class DateStorage {
+  constructor(month, day, year, start) {
+    this.month = month;
+    this.day = day;
+    this.year = year;
+    this.start = start
+  }
 
-DateStorage.prototype.weekday = function(input) {
-  let yearStart = 0
-  let leapYear = 0
-  let cycle = (this.year - 12 ) % 28
-  /* istanbul ignore else */
-  if (cycle % 4 === 0) {
-    leapYear = 1
-    if (cycle === 12) {
-      yearStart = 0
-    } else if (cycle === 24) {
-      yearStart = 1
-    } else if (cycle === 8) {
-      yearStart = 2
-    } else if (cycle === 20) {
-      yearStart = 3
-    } else if (cycle === 4) {
-      yearStart = 4
-    } else if (cycle === 16) {
-      yearStart = 5
-    } else if (cycle === 0) {
-      yearStart = 6
+  findYear() {
+    let yearStart = 0
+    let leapYear = 0
+    let cycle = (this.year - 12 ) % 28
+    if (cycle % 4 === 0) {
+      leapYear = 1
+      if (cycle === 12) {
+        yearStart = 0
+      } else if (cycle === 24) {
+        yearStart = 1
+      } else if (cycle === 8) {
+        yearStart = 2
+      } else if (cycle === 20) {
+        yearStart = 3
+      } else if (cycle === 4) {
+        yearStart = 4
+      } else if (cycle === 16) {
+        yearStart = 5
+      } else if (cycle === 0) {
+        yearStart = 6
+      }
+      this.start.push(leapYear, yearStart)
+      return this.start
+    } else {
+      if (cycle === 6 || cycle === 17 || cycle === 23) {
+        yearStart = 0
+      } else if(cycle === 1 || cycle === 7 || cycle === 18) {
+        yearStart = 1
+      } else if(cycle === 2 || cycle === 13 || cycle === 19) {
+        yearStart = 2
+      } else if(cycle === 3 || cycle === 14 || cycle === 25) {
+        yearStart = 3
+      } else if(cycle === 9 || cycle === 15 || cycle === 26) {
+        yearStart = 4
+      } else if(cycle === 10 || cycle === 21 || cycle === 27) {
+        yearStart = 5
+      } else if(cycle === 5 || cycle === 11 || cycle === 22) {
+        yearStart = 6
+      }
+      this.start.push(leapYear, yearStart)
+      return this.start
     }
-    let arr = [leapYear, yearStart]
-    return arr
-  } else {
-    if (cycle === 6 || cycle === 17 || cycle === 23) {
-      yearStart = 0
-    } else if(cycle === 1 || cycle === 7 || cycle === 18) {
-      yearStart = 1
-    } else if(cycle === 2 || cycle === 13 || cycle === 19) {
-      yearStart = 2
-    } else if(cycle === 3 || cycle === 14 || cycle === 25) {
-      yearStart = 3
-    } else if(cycle === 9 || cycle === 15 || cycle === 26) {
-      yearStart = 4
-    } else if(cycle === 10 || cycle === 21 || cycle === 27) {
-      yearStart = 5
-    } else if(cycle === 5 || cycle === 11 || cycle === 22) {
-      yearStart = 6
+  }
+
+  findMonth() {
+    let month = this.month
+    let arr = this.start
+    let monthStart = 0
+    if (month === 1) {
+      monthStart = 0
+    } else if (month === 2) {
+      monthStart = 3
     }
-    let arr = [leapYear, yearStart]
-    return arr
+    if (this.start[0] === 1) { 
+      if (month === 10) {
+        monthStart = 1
+      } else if (month === 5) {
+        monthStart = 2
+      } else if (month === 8) {
+        monthStart = 3
+      } else if (month === 3 || month === 11) {
+        monthStart = 4
+      } else if (month === 6) {
+        monthStart = 5
+      } else if (month === 9 || month === 12) {
+        monthStart = 6
+      } else if (month === 4 || month === 7) {
+        monthStart = 0
+      }
+    } else {
+      if (month === 10) {
+        monthStart = 0
+      } else if (month === 5) {
+        monthStart = 1
+      } else if (month === 8) {
+        monthStart = 2
+      } else if (month === 3 || month === 11) {
+        monthStart = 3
+      } else if (month === 6) {
+        monthStart = 4
+      } else if (month === 9 || month === 12) {
+        monthStart = 5
+      } else if (month === 4 || month === 7) {
+        monthStart = 6
+      }
+    }
+    let firstDayOfMonth = monthStart + arr[1]
+    if (firstDayOfMonth > 6) {
+      firstDayOfMonth -= 7
+    }
+    return firstDayOfMonth
   }
 }
